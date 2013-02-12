c7a7bb4ea72cd9867e753d87d6a646311b37e0fc
Jacob Kelley <jakie8@gmail.com>
2013-02-12 08:38:37 -0800

    Updated Schema

--- src/install.sql
+++ src/install.sql
@@ -3,7 +3,7 @@
 ############################
 
 CREATE TABLE `shadow_meta` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` bigint NOT NULL auto_increment,
   `namespace` varbinary(25) NOT NULL,
   `type` varchar(255) default NULL,
   `object_id` varchar(255) NOT NULL,
@@ -12,10 +12,10 @@ CREATE TABLE `shadow_meta` (
   `parent` int(11) default NULL,
   `expires` timestamp NULL default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
 
 CREATE TABLE `shadow_objects` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` bigint NOT NULL auto_increment,
   `operation` varchar(10) default NULL,
   `namespace` varchar(25) default NULL,
   `type` varchar(255) default NULL,
@@ -25,14 +25,14 @@ CREATE TABLE `shadow_objects` (
   `negative` int(11) NOT NULL default '0',
   `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
 
 CREATE TABLE `shadow_object_relations` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` bigint NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
   `real_object_id` int(11) NOT NULL,
   `value` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `user_id` (`user_id`),
   KEY `real_object_id` (`real_object_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
